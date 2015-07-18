c5ac31e60dea44004eb6eb6f6e60a428e5bdb718
alvarocalace <alvicala_10@hotmail.com>
2015-07-18 14:46:01 -0300

    Update osufollowers-latest.user.js

--- osufollowers-latest.user.js
+++ osufollowers-latest.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name osu! followers
-// @version 0.16
+// @version 0.17
 // @author Alvaro Daniel Calace
 // @namespace https://github.com/alvarocalace/osufollowers
 // @description Adds a new followed players section in your osu! profile
